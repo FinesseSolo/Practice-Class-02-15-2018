dff744242500d46eb09125bc09e173fcea5f7fa4
Hancock <bchancock.2017@mymail.jcsu.edu>
2018-02-15 13:25:16 -0500

    first

---
+++ EvenOdd.java
@@ -0,0 +1,21 @@
+
+public class EvenOdd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a = 8;
+
+int b = evenorodd (a);}
+
+
+public static int evenorodd (int num1){
+	
+
+if (num1 % 2 == 0){
+	System.out.println("True");
+}
+
+else
+	System.out.println("false");
+return num1;
+} }
---
+++ Multiplication.java
@@ -0,0 +1,16 @@
+
+public class Multiplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a = 25; int b = 23;
+
+int c = multiply (a,b);
+
+System.out.println("The answer is" + c);
+}
+public static int multiply (int num1, int num2){
+	return num1 * num2;
+
+}
+}
